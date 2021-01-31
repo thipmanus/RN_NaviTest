e84aa13b370fa4607c43886d8be40be46b29c343
thipmanus <5910110148@psu.ac.th>
2021-01-31 20:58:46 +0700

    [Add]BottomTab and Icon

---
+++ screens/TabOne.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TabOne() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>TabOne!</Text>
+    </View>
+  );
+}
---
+++ screens/TabThree.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TabThree() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>TabThree!</Text>
+    </View>
+  );
+}
